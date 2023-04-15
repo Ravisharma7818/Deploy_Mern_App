478ab007a564934d04bdd372cf08b6e25cc6ffda
Mukul1212 <102962459+Mukul1212@users.noreply.github.com>
2023-04-16 04:11:20 +0530

    change port

--- client/src/components/showTodoList.jsx
+++ client/src/components/showTodoList.jsx
@@ -34,7 +34,7 @@ export function ShowTodoList() {
     useEffect(
         function () {
             axios
-                .get("http://localhost:8000/api/todo")
+                .get("https://mern-deploy-20p6.onrender.com")
                 .then((res) => {
                     console.log(res.data);
                     setTodo(res.data);
@@ -57,7 +57,7 @@ export function ShowTodoList() {
     }
 
     function handleDelete(e) {
-        axios.delete(`http://localhost:8000/api/todo/${e.target.name}`);
+        axios.delete(`https://mern-deploy-20p6.onrender.com/${e.target.name}`);
 
         setTodo((data) => {
             return data.filter((todo) => todo._id !== e.target.name);
